fcd359a411bb236870365928cd595aaf568e3d4b
kagakububutyou <shinnnosuke1232@yahoo.co.jp>
2014-06-08 22:47:12 +0900

    空のウインドウを開く

--- 練習/main.cpp
+++ 練習/main.cpp
@@ -11,9 +11,13 @@ int main(int argc, char *argv[])
 {
 
 	//	ウインドウだけを表示
+	//	↓初期化して
 	glutInit(&argc, argv);
+	//	↓ウインドウを開く
 	glutCreateWindow(argv[0]);
+	//	↓そのウインドウ内に絵を描く関数を決めて
 	glutDisplayFunc(display);
+	//	↓何かことが起こるのを待つ
 	glutMainLoop();
 
 	return 0;
